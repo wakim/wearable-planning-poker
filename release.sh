61ed75250854f22183dd2d159cc6da6fa8a7c4a7
Wakim Jraige <wakim.jraige@gmail.com>
2014-09-13 23:25:20 -0300

    Alteracao para reducao de tamanho durante o build release

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 ./gradlew assembleRelease
 
 my_path=$(pwd)/
@@ -19,10 +21,27 @@ apk_location=$my_path$project_directory/build/outputs/apk/$project_directory-rel
 apk_location_aligned=$my_path$project_directory/build/outputs/apk/$project_directory-release.apk
 keystore_location=/home/$me/.android/$keystore_location.jks
 
-echo 'jarsigner'
+echo 'comprimindo o apk com maxima eficiencia'
 
+unzip "$apk_location" -d "$my_path$project_directory/build/outputs/apk/$project_directory-release-unsigned-uncompressed"
+
+echo 'Apagando o META-INF'
+
+rm -r -f "$my_path$project_directory/build/outputs/apk/$project_directory-release-unsigned-uncompressed/META-INF"
+
+echo 'Apagando APK antigo'
+rm -r -f "$my_path$project_directory/build/outputs/apk/$project_directory-release-unsigned.apk"
+
+echo 'Comprimindo novamente com maior eficiencia'
+7z a -tzip "$my_path$project_directory/build/outputs/apk/$project_directory-release-unsigned.apk" "$my_path$project_directory/build/outputs/apk/$project_directory-release-unsigned-uncompressed/*" -mm=Deflate -mpass=15 -mfb=258
+
+echo 'Apagando pasta temporaria'
+rm -r -f "$my_path$project_directory/build/outputs/apk/$project_directory-release-unsigned-uncompressed"
+
+echo 'jarsigner'
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$keystore_location" "$apk_location" $alias_name
 echo 'jarsigner finalizado'
 
+echo 'zipalign'
 /home/$me/android-studio/sdk/build-tools/20.0.0/zipalign -v 4 "$apk_location" "$apk_location_aligned"
 echo 'zipalign finalizado'
